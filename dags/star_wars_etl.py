4edb7a261e4e246b6e4b12ea7775108423283f7a
Andre Rizzo <110422672+rizzander@users.noreply.github.com>
2024-02-14 16:57:40 -0300

    3 comit

--- dags/star_wars_etl.py
+++ dags/star_wars_etl.py
@@ -4,9 +4,10 @@ from datetime import datetime, timedelta
 import json
 import requests
 import pandas as pd
-
+import os
+output_folder = './'
 # Define a função para obter todos os dados de um endpoint da API e agrupar por ano de criação
-def get_all_data_and_group_by_year(endpoint, directory):
+def get_data_and_group_by_year(endpoint, output_folder):
     data_by_year = {}
     page = 1
     while True:
@@ -27,52 +28,80 @@ def get_all_data_and_group_by_year(endpoint, directory):
             return
 
     for year, results in data_by_year.items():
-        filename = f"{directory}/{year}/{endpoint.split('/')[-2]}.json"
+        filename = f"{output_folder}/{year}/{endpoint.split('/')[-2]}.json"
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, 'w') as f:
             json.dump(results, f, indent=4)
 
 # Define a função para contar o número de registros do endpoint 'people'
-def count_records(**kwargs):
-    ti = kwargs['ti']  # Obtém o objeto TaskInstance
-    people_data = ti.xcom_pull(task_ids='extrair_dados')  # Obtém os dados da tarefa extrair_dados
-    num_records = sum(len(data) for data in people_data.values())  # Conta o número total de registros
-    print(f"O número total de registros do endpoint 'people' é: {num_records}")
+def count_records(people_endpoint, output_folder):
+    url = people_endpoint
+    num_records = 0
 
-# Define a função para extrair os títulos dos filmes vinculados a cada pessoa e salvá-los em um arquivo JSON
-def transform(**kwargs):
-    ti = kwargs['ti']  # Obtém o objeto TaskInstance
-    people_data = ti.xcom_pull(task_ids='extrair_dados')  # Obtém os dados da tarefa extrair_dados
-    
-    # Carrega os dados de filmes
-    films_data = pd.read_json(people_data['people'])  # Supondo que a saída da tarefa extrair_dados foi salva com a chave 'people'
-    
-    def extract_films_titles(films_urls):
-        films_titles = []
-        for film_url in films_urls:
-            film_id = film_url.split('/')[-2]
-            film_title = films_data[films_data['url'].str.contains(film_id)]['title'].iloc[0]
-            films_titles.append(film_title)
-        return films_titles
+    while url:
+        response = requests.get(url)
+        data = response.json()
+        num_records += len(data['results'])
+        url = data['next']
+    os.makedirs(output_folder, exist_ok=True)
+    with open(f"{output_folder}/contagem.txt", 'w') as f:
+        f.write(f"O número total de registros do endpoint 'people' é: {num_records}")
+    return num_records
 
-    people_films = []
-    for year_data in people_data.values():
-        for person_data in year_data:
-            films_titles = extract_films_titles(person_data['films'])
-            people_films.append({
-                'name': person_data['name'],
-                'gender': person_data['gender'],
-                'films': films_titles
-            })
+# Define a função para transformar os dados e salvar em um arquivo JSON
+def transform(people_endpoint, films_endpoint, output_folder):
+    # Definir os endpoints da API
+    people_endpoint = 'https://swapi.dev/api/people/'
+    films_endpoint = 'https://swapi.dev/api/films/'
 
-    with open('people_with_films.json', 'w') as outfile:
-        json.dump(people_films, outfile, indent=4)
+    # Requisição aos dados de pessoas
+    response_people = requests.get(people_endpoint)
+    people_data = response_people.json()
+
+    # Requisição aos dados de filmes
+    response_films = requests.get(films_endpoint)
+    films_data = response_films.json()
+
+    # Selecionar apenas os atributos desejados para as pessoas
+    transformed_data = []
+    for person_data in people_data['results']:
+        transformed_person = {
+            'name': person_data['name'],
+            'gender': person_data['gender'],
+            'films': []  # Inicialmente, nenhum título de filme associado
+        }
+
+        # Adicionar títulos de filmes vinculados à pessoa
+        for film_url in person_data['films']:
+            response_film = requests.get(film_url)
+            film_data = response_film.json()
+            transformed_person['films'].append(film_data['title'])
+
+        transformed_data.append(transformed_person)
+
+    # Selecionar apenas os atributos desejados para os filmes
+    transformed_films = []
+    for film_data in films_data['results']:
+        transformed_film = {
+            'title': film_data['title']
+        }
+        transformed_films.append(transformed_film)
+    # Montar o JSON final combinando pessoas e filmes
+    final_json = {
+        'people': transformed_data,
+        'films': transformed_films
+    }
+    os.makedirs(output_folder, exist_ok=True)
+    # Salvar os dados transformados em um arquivo JSON
+    with open(f"{output_folder}/transformed_data.json", 'w') as outfile:
+        json.dump(transformed_data, outfile, indent=4)
+
 
 # Definir argumentos padrão da DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 2, 12),
+    'start_date': datetime(2024, 2, 14),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -96,26 +125,43 @@ dag = DAG(
 )
 
 # Definir as tasks da DAG
-extract_data_task = PythonOperator(
-    task_id='extrair_dados',
-    python_callable=get_all_data_and_group_by_year,
-    op_args=[endpoints['people'], 'people'],
+extract_data_people_task = PythonOperator(
+    task_id='extrair_dados_people',
+    python_callable=get_data_and_group_by_year,
+    op_args=[endpoints['people'], output_folder],
+    dag=dag,
+)
+
+extract_data_films_task = PythonOperator(
+    task_id='extrair_dados_films',
+    python_callable=get_data_and_group_by_year,
+    op_args=[endpoints['films'], output_folder],
+    dag=dag,
+)
+
+extract_data_vehicles_task = PythonOperator(
+    task_id='extrair_dados_vehicles',
+    python_callable=get_data_and_group_by_year,
+    op_args=[endpoints['vehicles'], output_folder],
     dag=dag,
 )
 
 count_records_task = PythonOperator(
     task_id='contar_registros',
     python_callable=count_records,
-    provide_context=True, 
+    provide_context=True,
+    op_args=[endpoints['people'], output_folder], 
     dag=dag,
 )
 
 transform_task = PythonOperator(
     task_id='transformar',
     python_callable=transform,
-    provide_context=True,  
+    provide_context=True,
+    op_args=[endpoints['people'], endpoints['films'], output_folder],
     dag=dag,
 )
 
 # Definir as dependências entre as tasks
-extract_data_task >> count_records_task >> transform_task
+extract_data_people_task >> extract_data_films_task >> extract_data_vehicles_task >> count_records_task >> transform_task
+
